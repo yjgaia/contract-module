150a7eda067ca99f10c192a406a3f31ebcf1a7fd
yjgaia <yjdev88@gmail.com>
2024-10-05 11:14:17 +0900

    Move event tracker files to contract-event-tracker module

--- database/init.sql
+++
@@ -1,5 +0,0 @@
-SET default_tablespace = '';
-SET default_table_access_method = "heap";
-
-CREATE EXTENSION IF NOT EXISTS "pg_cron" WITH SCHEMA "extensions";
-CREATE EXTENSION IF NOT EXISTS "pg_net" WITH SCHEMA "extensions";
